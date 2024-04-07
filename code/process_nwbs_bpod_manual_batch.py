a8721aa40b8d22988b94482b1146e285326e764c
Han Hou <han.hou@alleninstitute.org>
2024-04-07 18:14:01 +0000

    feat(process nwb bpod): skip existing

--- code/process_nwbs_bpod_manual_batch.py
+++ code/process_nwbs_bpod_manual_batch.py
@@ -33,13 +33,35 @@ from process_nwbs import process_one_nwb
 
 logger = logging.getLogger(__name__)
 
+
+def get_nwb_to_process(nwb_folder, nwb_processed_folder):
+    # The simplest solution: find nwb files that have not been processed
+    nwb = [f_name.split('/')[-1].split('.')[0] for f_name in glob.glob(f'{nwb_folder}/*.nwb')]
+    nwb_processed = [f_name.split('/')[-1].split('.')[0] for f_name in glob.glob(f'{nwb_processed_folder}/*')]
+    
+    # Per this issue https://github.com/AllenNeuralDynamics/aind-foraging-behavior-bonsai-basic/issues/1, 
+    # I have to revert the processed file name back to json name, e.g.: 'xxxxx_2023-11-08_92908' to 'xxxxx_2023-11-08_09-29-08'
+    nwb_processed = [_reformat_string(s) for s in nwb_processed]
+
+    f_error = f'{nwb_processed_folder}/error_files.json'
+    if Path(f_error).exists():
+        nwb_errors = [f_name.split('/')[-1].split('.')[0] for f_name in json.load(open(f'{nwb_processed_folder}/error_files.json'))]
+    else:
+        nwb_errors = []
+        
+    nwb_to_process = [f'{nwb_folder}/{f}.nwb' for f in list(set(nwb) - set(nwb_processed) - set(nwb_errors))]
+        
+    return nwb_to_process
+    
+
 if __name__ == '__main__':
     
     data_folder = '/root/capsule/data/foraging_nwb_bpod'
     result_folder = '/root/capsule/results'
 
     # Make sure `foraging_nwb_bpod` is attached
-    nwb_file_names = glob.glob(f'{data_folder}/**/*.nwb', recursive=True)
+    # nwb_file_names = glob.glob(f'{data_folder}/**/*.nwb', recursive=True)
+    nwb_file_names = get_nwb_to_process(data_folder, '/root/capsule/data/foraging_nwb_bpod_processed')
     
     # DEBUG OVERRIDE
     # nwb_file_names = [f'{data_folder}/447921_2019-09-11_13-56-46.nwb']  # Test bpod session
