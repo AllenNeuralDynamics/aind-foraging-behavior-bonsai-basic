fe9346450758d06868e67135eb4b496b70882b25
Han Hou <han.hou@alleninstitute.org>
2024-04-07 18:42:06 +0000

    feat(process bpod): skip existing processed nwb

--- code/process_nwbs_bpod_manual_batch.py
+++ code/process_nwbs_bpod_manual_batch.py
@@ -18,6 +18,9 @@ import glob
 import os
 import logging
 import shutil
+from pathlib import Path
+import json
+import sys
 
 import multiprocessing as mp
 import tqdm
@@ -33,21 +36,41 @@ from process_nwbs import process_one_nwb
 
 logger = logging.getLogger(__name__)
 
+def _reformat_string(s):
+    if s.count('_') < 2:  # No suffix
+        return s
+    subject_date, time_part = s.rsplit('_', 1)
+    
+    if len(time_part) < 5:  # Old suffix (0, 1, 2, ...)
+        return s
+    
+    time_part = time_part.zfill(6)
+    formatted_time = f'{time_part[:2]}-{time_part[2:4]}-{time_part[4:6]}'
+    return f'{subject_date}_{formatted_time}'
 
-def get_nwb_to_process(nwb_folder, nwb_processed_folder):
+
+def get_nwb_to_process(nwb_folder, nwb_processed_folder, if_exclude_exist=True, if_exclude_error=True):
     # The simplest solution: find nwb files that have not been processed
     nwb = [f_name.split('/')[-1].split('.')[0] for f_name in glob.glob(f'{nwb_folder}/*.nwb')]
-    nwb_processed = [f_name.split('/')[-1].split('.')[0] for f_name in glob.glob(f'{nwb_processed_folder}/*')]
-    
-    # Per this issue https://github.com/AllenNeuralDynamics/aind-foraging-behavior-bonsai-basic/issues/1, 
-    # I have to revert the processed file name back to json name, e.g.: 'xxxxx_2023-11-08_92908' to 'xxxxx_2023-11-08_09-29-08'
-    nwb_processed = [_reformat_string(s) for s in nwb_processed]
+    logger.info(f'Total {len(nwb)} nwbs in {nwb_folder}')
+    logger.info(f'   examples: {nwb[:3]}')
+
+    if if_exclude_exist:
+        nwb_processed = [f_name.split('/')[-1].split('.')[0] for f_name in glob.glob(f'{nwb_processed_folder}/*')]
+        # Per this issue https://github.com/AllenNeuralDynamics/aind-foraging-behavior-bonsai-basic/issues/1, 
+        # I have to revert the processed file name back to json name, e.g.: 'xxxxx_2023-11-08_92908' to 'xxxxx_2023-11-08_09-29-08'
+        nwb_processed = [_reformat_string(s) for s in nwb_processed]
+        logger.info(f'{len(nwb_processed)} processed nwbs already in {nwb_processed_folder}')
+        logger.info(f'   examples: {nwb_processed[:3]}')
+    else:
+        nwb_processed = []
 
     f_error = f'{nwb_processed_folder}/error_files.json'
-    if Path(f_error).exists():
+    if if_exclude_error and Path(f_error).exists():
         nwb_errors = [f_name.split('/')[-1].split('.')[0] for f_name in json.load(open(f'{nwb_processed_folder}/error_files.json'))]
     else:
         nwb_errors = []
+    logger.info(f'previous {len(nwb_errors)} errors to skip')
         
     nwb_to_process = [f'{nwb_folder}/{f}.nwb' for f in list(set(nwb) - set(nwb_processed) - set(nwb_errors))]
         
@@ -61,7 +84,10 @@ if __name__ == '__main__':
 
     # Make sure `foraging_nwb_bpod` is attached
     # nwb_file_names = glob.glob(f'{data_folder}/**/*.nwb', recursive=True)
-    nwb_file_names = get_nwb_to_process(data_folder, '/root/capsule/data/foraging_nwb_bpod_processed')
+    nwb_file_names = get_nwb_to_process(
+        nwb_folder=data_folder,
+        nwb_processed_folder='/root/capsule/data/foraging_nwb_bpod_processed',
+    )
     
     # DEBUG OVERRIDE
     # nwb_file_names = [f'{data_folder}/447921_2019-09-11_13-56-46.nwb']  # Test bpod session
