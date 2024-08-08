f4e87edc75f0a655e4e1c9f81cacc5b8d7621d47
houhan@gmail.com <han.hou@alleninstitute.org>
2024-08-08 00:01:33 +0000

    feat: add mean and std scores

--- code/analysis/analysis_wrapper.py
+++ code/analysis/analysis_wrapper.py
@@ -46,7 +46,7 @@ def compute_logistic_regression(nwb) -> Tuple[
                                 selected_trial_idx=None,
                                 solver='liblinear', 
                                 penalty='l2',
-                                Cs=10,
+                                Cs=20,
                                 cv=10,
                                 n_jobs_cross_validation=1,
                                 n_bootstrap_iters=1000, 
@@ -65,6 +65,12 @@ def compute_logistic_regression(nwb) -> Tuple[
         df_session_logistic_regression.loc[
                     0, f'logistic_{model_name}_bias'
                 ] = dict_logistic_result['df_beta'].loc['bias']['cross_validation'].values[0]
+        
+        # Add goodness of fitting
+        model = dict_logistic_result['logistic_reg_cv']
+        ind_C = np.where(model.Cs_ == model.C_)[0]  # Get the ind of best C 
+        score_mean = np.mean(model.scores_[1.0][:, ind_C], axis=0)
+        score_std = np.std(model.scores_[1.0][:, ind_C], axis=0)
 
         dict_figures[model_name] = ax.figure
     
